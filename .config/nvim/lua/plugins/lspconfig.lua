2671bf765701723e412c6782060d7de4a86cc15e
giackperetti <github.giack@gmail.com>
2024-05-30 22:07:58 +0200

    disabled semantic tokens for gopls

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,6 +3,15 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              semanticTokens = false,
+            },
+          },
+        },
+      },
     },
   },
 }
