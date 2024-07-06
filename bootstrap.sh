9f02314f71c021343a2454ab2663dd5fcac4ae4b
giackperetti <github.giack@gmail.com>
2024-07-06 12:36:58 +0200

    added one dependency

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 check_package() {
-	if ! pacman -Q "$1" &>/dev/null; then
-		echo -e "Package $1 is not installed. Installing..."
-		sudo pacman -S --noconfirm "$1"
-	else
-		echo -e "Package $1 is already installed."
-	fi
+  if ! pacman -Q "$1" &>/dev/null; then
+    echo -e "Package $1 is not installed. Installing..."
+    sudo pacman -S --noconfirm "$1"
+  else
+    echo -e "Package $1 is already installed."
+  fi
 }
 
-required_packages=("git" "base-devel" "stow" "i3-wm" "i3status" "zsh" "tmux" "kitty" "neovim" "dunst" "zathura" "bat" "rofi" "picom" "ranger" "libqalculate")
+required_packages=("git" "base-devel" "stow" "i3-wm" "i3status" "zsh" "tmux" "kitty" "neovim" "dunst" "zathura" "bat" "rofi" "picom" "ranger" "libqalculate" "xdotool")
 
 # Check and install required packages
 for package in "${required_packages[@]}"; do
-	check_package "$package"
+  check_package "$package"
 done
 
 # Stow the dotfiles
