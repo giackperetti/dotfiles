b54159194d3f373a341750677f67d8a33a78c17e
giackperetti <github.giack@gmail.com>
2024-08-18 16:27:28 +0200

    bootstrap-arch.sh: added functionality to automatically install oh-my-zsh and the required plugins when setting up a new computer

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -11,10 +11,31 @@ check_package() {
 
 required_packages=("git" "base-devel" "stow" "i3-wm" "i3status" "zsh" "tmux" "kitty" "neovim" "dunst" "zathura" "bat" "rofi" "picom" "ranger" "libqalculate" "xdotool" "maim")
 
-# Check and install required packages
+# Install all required packages
 for package in "${required_packages[@]}"; do
   check_package "$package"
 done
+
+# Install oh-my-zsh and required plugins
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Oh My Zsh is not installed. Installing..."
+
+  # Install oh-my-zsh itself
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  # Install required plugins
+  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+else
+  echo "Oh My Zsh is already installed."
+fi
+
+# Set ZSH as default shell
+if [ "$SHELL" != "$(which zsh)" ]; then
+  echo "Changing default shell to ZSH."
+  chsh -s "$(which zsh)"
+else
+  echo "Default shell is already ZSH."
+fi
 
 # Stow the dotfiles
 stow .
