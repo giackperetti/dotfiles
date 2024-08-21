272cef438ef75dd3f8b8bae95ab22090667a3807
giackperetti <github.giack@gmail.com>
2024-08-21 19:21:52 +0200

    neovim: added better Elixir support, moved Neotree to the right

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.elixir" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.formatting.black" },
     -- import/override with your plugins
