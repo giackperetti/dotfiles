199e6404b888dabc6cdddca699fb978c3c5f64f5
giackperetti <github.giack@gmail.com>
2024-09-23 08:34:14 +0200

    nvim: added php support

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,10 +7,10 @@ vim.opt.timeout = true
 vim.opt.timeoutlen = 0
 
 -- Set default tab size
-vim.opt.tabstop = 2
+vim.opt.tabstop = 4
 vim.opt.expandtab = true
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 -- Make sure Nerd Fonts are used
 vim.g.have_nerd_font = true
@@ -18,5 +18,8 @@ vim.g.have_nerd_font = true
 -- Disable spell checking
 vim.opt.spell = false
 
--- Use ruff as python lsp
+-- Use pyright as python lsp
 vim.g.lazyvim_python_lsp = "pyright"
+
+-- Use intelephense as php lsp
+vim.g.lazyvim_php_lsp = "intelephense"
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,17 +1,37 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = { enabled = false },
-      servers = {
-        gopls = {
-          settings = {
-            gopls = {
-              semanticTokens = false,
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            inlay_hints = { enabled = false },
+            servers = {
+                gopls = {
+                    settings = {
+                        gopls = {
+                            semanticTokens = false,
+                        },
+                    },
+                },
+                emmet_ls = {
+                    filetypes = {
+                        "astro",
+                        "css",
+                        "eruby",
+                        "html",
+                        "htmldjango",
+                        "javascriptreact",
+                        "less",
+                        "php",
+                        "pug",
+                        "sass",
+                        "scss",
+                        "svelte",
+                        "typescriptreact",
+                        "vue",
+                        "eex",
+                        "heex",
+                    },
+                },
             },
-          },
         },
-      },
     },
-  },
 }
