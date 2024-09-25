f7e81fa0ce35917aef40bee928e4df13d08bb305
giackperetti <github.giack@gmail.com>
2024-09-25 13:51:50 +0200

    neovim: enabled cssls in all relevant filetypes

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,6 +3,7 @@ return {
         "neovim/nvim-lspconfig",
         opts = {
             inlay_hints = { enabled = false },
+            -- document_highlight = { enabled = false },
             servers = {
                 gopls = {
                     settings = {
@@ -12,6 +13,26 @@ return {
                     },
                 },
                 emmet_ls = {
+                    filetypes = {
+                        "astro",
+                        "css",
+                        "eruby",
+                        "html",
+                        "htmldjango",
+                        "javascriptreact",
+                        "less",
+                        "php",
+                        "pug",
+                        "sass",
+                        "scss",
+                        "svelte",
+                        "typescriptreact",
+                        "vue",
+                        "eex",
+                        "heex",
+                    },
+                },
+                cssls = {
                     filetypes = {
                         "astro",
                         "css",
